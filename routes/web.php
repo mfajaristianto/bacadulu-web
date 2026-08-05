a97a7dcce3c59054781a16a7bd034f5c48829f9f
Nama Teman <farel170408@gmail.com>
2026-08-05 10:40:45 +0700

    tes perbaikan

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::middleware(['auth', 'admin'])->group(function () {
+    Route::middleware(['auth'])->group(function () {
         Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
         Route::resource('informations', InformationAdminController::class);
         Route::resource('journals', \App\Http\Controllers\Admin\JurnalAdminController::class);
